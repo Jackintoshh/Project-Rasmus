3aaccedc5e82f5c62ba6e08f43a05e0baf1e19c7
jackintoshh <jjwright10@hotmail.com>
2018-04-22 22:02:44 +0100

    Game over state added

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    
+
+
+    Animator anim;
+    
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+
+            
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene(0);
+            }
+            
+            
+        }
+    }
+}
