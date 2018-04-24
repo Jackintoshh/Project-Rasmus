84132c0f8058e60c97a21f3d590c4abf55239d3d
Jackintoshh <jjwright10@hotmail.com>
2018-04-24 14:23:24 +0100

    Added ammo

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -5,6 +5,7 @@ public class PlayerShooting : MonoBehaviour
     public int damagePerShot = 20;
     public float timeBetweenBullets = 0.15f;
     public float range = 100f;
+    public static int ammo = 30;
 
 
     float timer;
@@ -15,7 +16,8 @@ public class PlayerShooting : MonoBehaviour
     LineRenderer gunLine;
     //AudioSource gunAudio;
     Light gunLight;
-    //float effectsDisplayTime = 0.2f;
+    float effectsDisplayTime = 0.2f;
+    //int ammo = 30;
 
 
     void Awake ()
@@ -37,7 +39,7 @@ public class PlayerShooting : MonoBehaviour
             Shoot ();
         }
 
-        if(timer >= timeBetweenBullets)// * effectsDisplayTime)
+        if(timer >= timeBetweenBullets * effectsDisplayTime)
         {
             DisableEffects ();
         }
@@ -53,33 +55,43 @@ public class PlayerShooting : MonoBehaviour
 
     void Shoot ()
     {
-        timer = 0f;
-
-        //gunAudio.Play ();
-
-        gunLight.enabled = true;
-
-        //gunParticles.Stop ();
-        //gunParticles.Play ();
-
-        gunLine.enabled = true;
-        gunLine.SetPosition (0, transform.position);
-
-        shootRay.origin = transform.position;
-        shootRay.direction = transform.forward;
-
-        if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+        if (ammo <= 0)
         {
-            EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
-            if(enemyHealth != null)
-            {
-                enemyHealth.TakeDamage (damagePerShot, shootHit.point);
-            }
-            gunLine.SetPosition (1, shootHit.point);
+            return;
         }
         else
         {
-            gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+
+            ammo--;
+            AmmoManager.ammo--;
+            timer = 0f;
+
+            //gunAudio.Play ();
+
+            gunLight.enabled = true;
+
+            //gunParticles.Stop ();
+            //gunParticles.Play ();
+
+            gunLine.enabled = true;
+            gunLine.SetPosition(0, transform.position);
+
+            shootRay.origin = transform.position;
+            shootRay.direction = transform.forward;
+
+            if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+            {
+                EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
+                if (enemyHealth != null)
+                {
+                    enemyHealth.TakeDamage(damagePerShot, shootHit.point);
+                }
+                gunLine.SetPosition(1, shootHit.point);
+            }
+            else
+            {
+                gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+            }
         }
     }
 }
